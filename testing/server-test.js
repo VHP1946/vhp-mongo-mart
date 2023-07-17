e66485c628c6248729cac591020cd0749e1b984a
Christian Vogel <christianv@vogelheating.com>
2023-07-17 15:09:18 -0500

    added froot and sold to quote scheme

--- testing/server-test.js
+++ testing/server-test.js
@@ -4,10 +4,19 @@ let API = new Core({
         user:"VOGCH", 
         pswrd:"vogel123"
     },
-    host:'http://localhost:8080/',
+    host:'http://18.191.223.80/',
     sync:false, 
     dev:{comments:true}
 });
+let oldMart = new Core({
+  auth:{
+      user:"VOGCH", 
+      pswrd:"vogel123"
+  },
+  sync:false,
+  dev:{comments:true,https:true}
+
+})
 
 let fpack={
   db:'Quotes',
@@ -54,6 +63,19 @@ let ipack={
   }
 }
 
-API.SENDrequest({pack:fpack}).then(answer=>{
-    console.log(answer)
+//API.SENDrequest({pack:fpack,route:'STORE'}).then(answer=>{
+//    console.log(answer)
+//});
+
+let quote = {
+  collect:'apps',
+  db:'mquotes',
+  store:'RRQ',
+  method:'query',
+  options:{
+    query:{}
+  }
+}
+oldMart.SENDrequest({pack:quote,route:'STORE',request:'MART'}).then(answr=>{
+  console.log(answr.body.result);
 });
